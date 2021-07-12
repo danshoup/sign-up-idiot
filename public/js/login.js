3fdf692cae4dfb32d46dcc7eb91a7b8961c9531d
Diana Daghlas <dianadaghlas@yahoo.com>
2021-07-12 19:23:50 -0400

    Updated all code for the js files in public directory

--- public/js/login.js
+++ public/js/login.js
@@ -15,7 +15,7 @@ const loginFormHandler = async (event) => {
   
       if (response.ok) {
         // If successful, redirect the browser to the profile page
-        document.location.replace('/userDashboard');
+        document.location.replace('/userProfile');
       } else {
         alert(response.statusText);
       }
@@ -37,7 +37,7 @@ const loginFormHandler = async (event) => {
       });
   
       if (response.ok) {
-        document.location.replace('/userDashboard');
+        document.location.replace('/userProfile');
       } else {
         alert(response.statusText);
       }
--- public/js/profile.js
+++ public/js/profile.js
@@ -1,14 +1,15 @@
 const newFormHandler = async (event) => {
     event.preventDefault();
   
-    const name = document.querySelector('#project-name').value.trim();
-    const needed_funding = document.querySelector('#project-funding').value.trim();
-    const description = document.querySelector('#project-desc').value.trim();
+    const name = document.querySelector('#event-name').value.trim();
+    const event_date = document.querySelector('#event-date').value.trim();
+    const event_address = document.querySelector('#event-address').value.trim();
+    const event_owner = document.querySelector('#event-owner').value.trim();
   
-    if (name && needed_funding && description) {
-      const response = await fetch(`/api/projects`, {
+    if (name && event_date && event_address && event_owner) {
+      const response = await fetch(`/api/events`, {
         method: 'POST',
-        body: JSON.stringify({ name, needed_funding, description }),
+        body: JSON.stringify({ name, event_date, event_address, event_owner }),
         headers: {
           'Content-Type': 'application/json',
         },
@@ -17,7 +18,7 @@ const newFormHandler = async (event) => {
       if (response.ok) {
         document.location.replace('/profile');
       } else {
-        alert('Failed to create project');
+        alert('Failed to create event');
       }
     }
   };
@@ -26,23 +27,71 @@ const newFormHandler = async (event) => {
     if (event.target.hasAttribute('data-id')) {
       const id = event.target.getAttribute('data-id');
   
-      const response = await fetch(`/api/projects/${id}`, {
+      const response = await fetch(`/api/events/${id}`, {
         method: 'DELETE',
       });
   
       if (response.ok) {
         document.location.replace('/profile');
       } else {
-        alert('Failed to delete project');
+        alert('Failed to delete event');
       }
     }
   };
   
   document
-    .querySelector('.new-project-form')
+    .querySelector('.new-event-form')
     .addEventListener('submit', newFormHandler);
   
   document
-    .querySelector('.project-list')
+    .querySelector('.event-list')
     .addEventListener('click', delButtonHandler);
-  
+    const newFormHandler = async (event) => {
+      event.preventDefault();
+    
+      const name = document.querySelector('#event-name').value.trim();
+      const event_date = document.querySelector('#event-date').value.trim();
+      const event_address = document.querySelector('#event-address').value.trim();
+      const event_owner = document.querySelector('#event-owner').value.trim();
+    
+      if (name && event_date && event_address && event_owner) {
+        const response = await fetch(`/api/events`, {
+          method: 'POST',
+          body: JSON.stringify({ name, event_date, event_address, event_owner }),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+    
+        if (response.ok) {
+          document.location.replace('/profile');
+        } else {
+          alert('Failed to create event');
+        }
+      }
+    };
+    
+    const delButtonHandler = async (event) => {
+      if (event.target.hasAttribute('data-id')) {
+        const id = event.target.getAttribute('data-id');
+    
+        const response = await fetch(`/api/events/${id}`, {
+          method: 'DELETE',
+        });
+    
+        if (response.ok) {
+          document.location.replace('/profile');
+        } else {
+          alert('Failed to delete event');
+        }
+      }
+    };
+    
+    document
+      .querySelector('.new-event-form')
+      .addEventListener('submit', newFormHandler);
+    
+    document
+      .querySelector('.event-list')
+      .addEventListener('click', delButtonHandler);
+    
