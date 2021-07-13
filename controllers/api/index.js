812a7854ffda2f8405bbc9963c7b48ee77061ba3
Diana Daghlas <dianadaghlas@yahoo.com>
2021-07-13 18:41:19 -0400

    Updated all contoller files with correct GET, POST, PUT, DELETE

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,11 @@
 const router = require('express').Router();
+
 const userRoutes = require('./userRoutes');
+const eventRoutes = require('./eventRoutes');
+const taskRoutes = require('./taskRoutes');
 
 router.use('/users', userRoutes);
+router.use('/posts', eventRoutes);
+router.use('/comments', taskRoutes);
 
 module.exports = router;
