52cd86cb418bb4effb17e16ce1ca68d895b29543
Diana Daghlas <dianadaghlas@yahoo.com>
2021-07-13 17:36:08 -0400

    Refactored controller index.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,15 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
-const homeRoutes = require('./homeRoutes');
+const homeRoutes = require('./homeRoutes.js');
+const profileRoutes = require('./profileRoutes.js');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
+router.use('/profile', profileRoutes);
+
+router.use((req, res) => {
+    res.status(404).end();
+  });
 
 module.exports = router;
