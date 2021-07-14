3c0660990ead3349b7ff63816cccc1f5ab75c7a1
Diana Daghlas <dianadaghlas@yahoo.com>
2021-07-14 13:42:23 -0400

    Removed unnecessary syntax from controllers index.js

--- controllers/index.js
+++ controllers/index.js
@@ -8,8 +8,4 @@ router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
 router.use('/profile', profileRoutes);
 
-router.use((req, res) => {
-    res.status(404).end();
-  });
-
 module.exports = router;
