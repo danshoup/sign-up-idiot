3f160047489a403edd6d2ed1c6df0ac2ab00e01d
Diana Daghlas <dianadaghlas@yahoo.com>
2021-07-17 11:31:49 -0400

    Refactoring newEvent and newTask

--- public/js/addEvent.js
+++ public/js/addEvent.js
@@ -1,20 +1,28 @@
-$('#datepicker').datepicker('getDate');
-
 const newEventFormHandler = async (event) => {
   event.preventDefault();
   
-    // Method should reflect in newEvent.handlebars
-    const name = document.querySelector('input[name="event-name"]').value; 
-    const event_date = document.querySelector('input[name="event-date"]').value;
-    const event_address = document.querySelector('input[name="event-address"]').value;
+    // Connects with newEvent.handlebars
+    const name = document.querySelector('#inputEventName"]').value; 
+    const event_start_date = document.querySelector('#eventStart').value;
+    const event_end_date = document.querySelector('#eventEnd').value;
+    const event_address_line1 = document.querySelector('#inputAddress').value;
+    const event_address_line2 = document.querySelector('#inputAddress2').value;
+    const event_address_city = document.querySelector('#inputCity').value;
+    const event_address_state = document.querySelector('#inputState').value;
+    const event_address_zip = document.querySelector('#inputZip]').value;
   
-  if (name && event_date && event_address){
+  if (name && event_start_date && event_end_date && event_address_line1 && event_address_line2 && event_address_city && event_address_state && event_address_zip){
     const response = await fetch(`/api/events`, {
       method: 'POST',
       body: JSON.stringify({
         name,
-        event_date,
-        event_address,
+        event_start_date,
+        event_end_date,
+        event_address_line1,
+        event_address_line2,
+        event_address_city,
+        event_address_state,
+        event_address_zip
       }),
     headers: {
       'Content-Type': 'application/json'
--- public/js/addTask.js
+++ public/js/addTask.js
@@ -1,10 +1,11 @@
 const newTaskFormHandler = async (event) => {
   event.preventDefault();
   
-    const name = document.querySelector('textarea[name="task-body"]').value.trim();
-    const event_id = window.location.toString().split('/')[
-      window.location.toString().split('/').length - 1
-    ];
+    const name = document.querySelector('#task-body').value.trim();
+    const event_id = JSON.parse(note.parentElement.getAttribute('data-event-id')).id;
+    // const event_id = window.location.toString().split('/')[
+    //   window.location.toString().split('/').length - 1
+    // ];
   
     if (name && event_id) {
         const response = await fetch('/api/tasks', {
