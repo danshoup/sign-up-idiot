45ff46d7c4697b12f0d5c9ca0ccb32c1b115d9ee
Diana Daghlas <dianadaghlas@yahoo.com>
2021-07-17 21:21:19 -0400

    Adjusted addEvent.js replace file locations

--- public/js/addEvent.js
+++ public/js/addEvent.js
@@ -2,18 +2,17 @@ const newEventFormHandler = async (event) => {
   event.preventDefault();
 
     // Connects with newEvent.handlebars
-    const name = document.querySelector('#inputEventName"]').value; 
-    const event_start_date = document.querySelector('#eventStart').value;
-    const event_end_date = document.querySelector('#eventEnd').value;
-    const event_address_line1 = document.querySelector('#inputAddress').value;
-    const event_address_line2 = document.querySelector('#inputAddress2').value;
-    const event_address_city = document.querySelector('#inputCity').value;
-    const event_address_state = document.querySelector('#inputState').value;
-
-    const event_address_zip = document.querySelector('#inputZip]').value;
+    const name = document.querySelector('#inputEventName"]').value.trim(); 
+    const event_start_date = document.querySelector('#eventStart').value.trim();
+    const event_end_date = document.querySelector('#eventEnd').value.trim();
+    const event_address_line1 = document.querySelector('#inputAddress').value.trim();
+    const event_address_line2 = document.querySelector('#inputAddress2').value.trim();
+    const event_address_city = document.querySelector('#inputCity').value.trim();
+    const event_address_state = document.querySelector('#inputState').value.trim();
+    const event_address_zip = document.querySelector('#inputZip]').value.trim();
   
   if (name && event_start_date && event_end_date && event_address_line1 && event_address_line2 && event_address_city && event_address_state && event_address_zip){
-    const response = await fetch(`/api/events`, {
+    const response = await fetch(`/api/event`, {
       method: 'POST',
       body: JSON.stringify({
         name,
