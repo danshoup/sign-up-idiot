1e0d429102951a42939b7ca44e2eb25ce2b8e293
Diana Daghlas <dianadaghlas@yahoo.com>
2021-07-18 23:26:43 -0400

    created editProfile.js to connect with editProfile.handlebars; code is referenced using the signup.js

---
+++ public/js/editProfile.js
@@ -0,0 +1,32 @@
+const editProfileFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const first_name = document.querySelector('#firstName-signup').value.trim();
+    const last_name = document.querySelector('#lastName-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+    const password_confirm = document.querySelector('#password-confirm').value.trim();
+  
+    if (first_name && last_name && email && (password === password_confirm)) {
+      const response = await fetch('/api/users', {
+        method: 'post',
+        body: JSON.stringify({
+          first_name,
+          last_name,
+          email,
+          password
+        }),
+        headers: { 'Content-Type': 'application/json' }
+      });
+  
+      // check the response status
+      if (response.ok) {
+        console.log('success');
+        document.location.replace('/userProfile');
+      } else {
+        alert(response.statusText);
+      }
+    }
+}
+  
+document.querySelector('.edit-profile-btn').addEventListener('submit', editProfileFormHandler);
